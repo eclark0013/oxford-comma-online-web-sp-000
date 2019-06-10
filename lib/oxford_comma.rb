ca0c4f0c09cf5b858c8b4caf1fe70f1e610a501a
Eric Clark <eclark0013@gmail.com>
2019-06-10 18:35:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,20 @@
 def oxford_comma(array)
-penult_array = array.pop
-empty_string=""
+  empty_string=""
+#only 1 element
+if array.size = 1
+  empty_string=array.join
+#end only 1 element
+
+#start 2 or more elements
+else
+penult_array = array.pop #making array out of n-1 elements (without losing last element)
+#start iteration
 penult_array.each do |element|
   empty_string<<"#{element}, "
 end
-empty_string<<"and #{array.last}"
+#end iteration
+empty_string<<"and #{array.last}" #put on the last element
+end
+#end 2 or more elements 
+
 end
