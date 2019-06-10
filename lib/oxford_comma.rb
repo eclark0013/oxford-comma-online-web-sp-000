e991e3249a5f3ba80a2fc43057214dfb2802bf20
Eric Clark <eclark0013@gmail.com>
2019-06-10 18:50:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,13 @@ if array.count == 1
   empty_string=array.join
 #end only 1 element
 
-#start 2 or more elements
-else
 penult_array = array.pop #making array out of n-1 elements (without losing last element)
+
+#start 2 or more elements
+elsif array.count==2
+  empty_string = "#{array[1]} and #{array[2]}"
 #start iteration
+else
 penult_array.each do |element|
   empty_string<<"#{element}, "
 end
@@ -16,5 +19,5 @@ end
 empty_string<<"and #{array.last}" #put on the last element
 end
 #end 2 or more elements
-
+empty_string
 end
